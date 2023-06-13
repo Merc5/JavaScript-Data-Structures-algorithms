25c3140e11cf16baa694d034825d849810e3a9fe
Kenneth Siyabonga Ncube <102526370+Merc5@users.noreply.github.com>
2023-06-13 08:52:33 +0100

    Update js-basics.js

--- js-basics.js
+++ js-basics.js
@@ -3,4 +3,15 @@
 /* This is a 
 multi-line comments */
 
+/////////////////////////////////////////////////
+
 // Data types and variables
+/* Data types
+udefined, null, boolean, string, symbol, number and object
+*/
+
+var myName = "KING";
+
+/////////////////////////////////////////////////
+
+// String Values with Assignmemt Operator
